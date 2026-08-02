ed1442d7a3243da8ed24b9966e6e279fbe59691a
jmm583 <jon.marchand4@gmail.com>
2026-08-01 20:38:57 -0400

    user interface generates an http_request_body for each of 3 options (unit convert, distance, study score)

--- test_client/user_interface.js
+++ test_client/user_interface.js
@@ -5,9 +5,9 @@ const rl = readline.createInterface({
 	output: process.stdout
 });
 
-const microsrvcPrompt = (askService) => {
+const userPrompt = (question) => {
 	return new Promise((resolve) => {
-		rl.question(askService, (answer) => {
+		rl.question(question, (answer) => {
 		resolve(answer);
 		});
 	});
@@ -15,10 +15,10 @@ const microsrvcPrompt = (askService) => {
 
 const main = async () => {
   while (true) {
-    const srvcChoice = await microsrvcPrompt(`
-        ==========================================
-        	Calculator Microservice Test Client
-        ==========================================
+    const serviceChoice = await userPrompt(`
+        =====================================
+        Calculate Microservice User Interface
+        =====================================
 
         Please choose an operation:
 
@@ -28,25 +28,11 @@ const main = async () => {
         4. Exit
 
         Choice: `);
-	
-	// initailize request_service variable to null
-	let request_service = null;
 
-	// set request_service based on user choice
-	if (srvcChoice === '1') {
-			request_service = "unit_conversion";
-		} else if (srvcChoice === '2') {
-			request_service = "lat_long_distance";
-		} else if (srvcChoice === '3') {
-			request_service = "study_score_calculation";
-		};
-
-	console.log(`Requesting service: ${request_service}`);
-
-    switch (srvcChoice) {
+    switch (serviceChoice) {
 		case '1':
-		// call unit conversion microservice
-			const unitConversionChoice = await microsrvcPrompt(`
+		// prompts user for unit conversion operation and value to convert
+			const unitConversionChoice = await userPrompt(`
 				Please choose a unit conversion operation:
 
 					1. Meters to Feet
@@ -63,6 +49,7 @@ const main = async () => {
 			console.log(`You chose: ${unitConversionChoice}`);
 			
 			// Correct naming convention for the value to convert using npm module convert-units
+			// preps the http request body for the unit conversion operation
 			const conversionOperations = {
 				'1': { from: "m",  to: "ft" },
 				'2': { from: "ft", to: "m" },
@@ -78,27 +65,65 @@ const main = async () => {
 			const conversionOperation = conversionOperations[unitConversionChoice];
 
 			// prompt user for the value to convert
-			const valueToConvert = await microsrvcPrompt(`Enter the value to convert from ${conversionOperation.from} to ${conversionOperation.to}: `);
-
-			const http_req_body = {
+			const valueToConvert = await userPrompt(`Enter the value to convert from ${conversionOperation.from} to ${conversionOperation.to}: `);
+			
+			// creates object to hold the unit conversion operation and value to convert
+			// converts value to float before sending HTTP request body
+			const http_req_body_convert = {
 				operation: conversionOperation,
-				value: valueToConvert,
+				value: parseFloat(valueToConvert),
 				unitFrom: conversionOperation.from,
 				unitTo: conversionOperation.to
 			};
-			console.log(http_req_body);
+			console.log(http_req_body_convert);
         break;
 
 		case '2':
-		// call latitude/longitude distance microservice
+			// prompts user for latitude and longitude of two points
+			const latitude1 = await userPrompt('Enter the latitude of the first point: ');
+			const longitude1 = await userPrompt('Enter the longitude of the first point: ');
+			const latitude2 = await userPrompt('Enter the latitude of the second point: ');
+			const longitude2 = await userPrompt('Enter the longitude of the second point: ');
+
+
+			// creates object to hold latitude and longitude inputs
+			// converts latitude and longitude to float before sending HTTP request body
+			const http_req_body_dist = {
+				operation: "distance", 
+				startLatitude: parseFloat(latitude1),
+				startLongitude: parseFloat(longitude1),
+				endLatitude: parseFloat(latitude2),
+				endLongitude: parseFloat(longitude2)
+			};
+			console.log(http_req_body_dist);
 		break;
 
 		case '3':
-		// call study score calculation microservice
+		// prompts user for study score calculation inputs
+			console.log('Study Score Calculation:');
+			console.log('Please enter the following information:');
+			const studyScore = await userPrompt('Enter the study score (0 to 5): ');
+			const wifiScore = await userPrompt('Enter the WiFi score (0 to 5): ');
+			const noiseScore = await userPrompt('Enter the noise score (0 to 5): ');
+			const seatingScore = await userPrompt('Enter the seating score (0 to 5): ');
+			const outletScore = await userPrompt('Enter the outlet score (0 to 5): ');
+			const overallRating = await userPrompt('Enter the overall rating (0 to 5): ');
+			
+			// creates object to hold the study score calculation inputs
+			// converts scores to integer before sending the HTTP request body
+			const http_req_body_study = {
+				operation: "Study Score Calculation",
+				studyScore: parseInt(studyScore),
+				wifiScore: parseInt(wifiScore),
+				noiseScore: parseInt(noiseScore),
+				seatingScore: parseInt(seatingScore),
+				outletScore: parseInt(outletScore),
+				overallRating: parseInt(overallRating)
+			};
+			console.log(http_req_body_study);
 		break;
 
 		case '4':
-
 			console.log('Exiting...');
 			rl.close();
 			return;
@@ -107,7 +132,7 @@ const main = async () => {
 		console.log('Invalid choice. Please try again.');
 		}
 	}
-  console.log(srvcChoice);
+  console.log(serviceChoice);
 };
 
 main();
