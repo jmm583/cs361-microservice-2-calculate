97df006ef3e2e7ec4454c81b440ed2947fc99b70
jmm583 <jon.marchand4@gmail.com>
2026-08-02 01:47:42 -0400

    successfully passed the operation key through async function from user_interface to the controller

--- calculator_microservice/src/controllers/CalculateController.js
+++ calculator_microservice/src/controllers/CalculateController.js
@@ -2,49 +2,12 @@ const unitConversionService = require("../services/UnitConversionService");
 const distanceService = require("../services/DistanceService");
 const studyScoreService = require("../services/StudyScoreService");
 
-const calculate = async (req, res) => { 
 
-    const url = "http://localhost:3000";
+async function calculate (req, res) {
+    console.log(req.body);
+    console.log(req.body.operation)
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response Status: ${response.status}`);
-        }
-
-        const result = await response.json();
-        console.log(result);
-    } catch (error) {
-        console.error(error.message);
-    }
+    res.json({message: "hit controller"})
 }
-
-    // if ( microServiceOperation === 'convert' ) {
-
-    //     console.log(req.body);
-    //     res.json({message: "user Interface reached controller"})
-
-    // } else if ( microServiceOperation === 'distance ') {
-        
-    //     console.log(req.body);
-    //     res.json({message: "user Interface reached controller"})
-
-    // } else if ( microServiceOperation === 'studyScore' ) {
-
-    //     console.log(req.body);
-    //     res.json({message: "user Interface reached controller"})
-    // }
-
-
-
-
-// test for controller
-// const calculate = (req, res) => {
-//     console.log(req.body);
-
-//     res.json({
-//         message: "hit controller"
-//     })
-// }
 
 module.exports = {calculate};
